e31d52de4adce195c8a8365d91e1add2818ae133
raimusthafa <raiimusthafa@gmail.com>
2025-10-29 23:44:08 +0500

    headers support formdata

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,6 +13,12 @@ api.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+  
+  // If sending FormData, remove Content-Type to let browser set it with boundary
+  if (config.data instanceof FormData) {
+    delete config.headers['Content-Type'];
+  }
+  
   return config;
 });
 
